c68d019a83663406f782b110a81e3ccd85275b9e
Fabian Soriano <fabo.soriano@gmail.com>
2023-01-10 20:51:10 -0800

    Added solution

--- solution1.py
+++ solution1.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 1 below
+def count_down(n):
+    # Base case
+    if n == 0:
+        return
+
+    # Recursive case
+    else:
+        print(n)
+        count_down(n-1)
+
+# Testing
+count_down(8)
--- solution2.py
+++ solution2.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 2 below
+def natural_nums(firstnum, secondnum):
+    # Base case
+    # If the number we are trying to print is greater than n (secondnum), stop the function.
+    if firstnum > secondnum:
+        return
+        
+    else:
+        print(firstnum)
+        natural_nums(firstnum+1, secondnum)
+
+# natural numbers will always start at 1 and increase as while numbers, 1, 2, 3...
+natural_nums(8)
